f9916330ec1a5421277af2b41fe69b25f6105787
zhaojianfeng <zhaojianfeng@strangerschat.cn>
2023-10-08 07:05:07 +0800

    update

--- chatDemo/src/main/java/chatDemo/controller/SecurityController.java
+++ chatDemo/src/main/java/chatDemo/controller/SecurityController.java
@@ -2,6 +2,9 @@ package chatDemo.controller;
 
 import javax.servlet.http.HttpSession;
 
+import chatDemo.common.AppContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,6 +19,7 @@ import chatDemo.service.SecurityService;
 @RequestMapping("/")
 public class SecurityController {
 
+    private final Logger logger = LoggerFactory.getLogger(SecurityController.class);
     @Autowired
     SecurityService securityService;
     
@@ -29,12 +33,15 @@ public class SecurityController {
     public ResponseJson login(HttpSession session,
             @RequestParam String username,
             @RequestParam String password) {
+        logger.info("收到登录请求：username= " + username + ". password= " + password);
         return securityService.login(username, password, session);
     }
     
     @RequestMapping(value = "logout", method = RequestMethod.POST)
     @ResponseBody
     public ResponseJson logout(HttpSession session) {
+
+        logger.info("用户登出：username= " + session.getAttribute("username") );
         return securityService.logout(session);
     }
 }
--- chatDemo/temp/cpfile.sh
+++ chatDemo/temp/cpfile.sh
@@ -1,1 +1,3 @@
 scp ../target/chatdemo.war ali1:/usr/local/tomcat/chatdemo/apache-tomcat-9.0.24/webapps/
+
+scp ./target/chatdemo.war ali1:/usr/local/tomcat/chatdemo/apache-tomcat-9.0.24/webapps/
